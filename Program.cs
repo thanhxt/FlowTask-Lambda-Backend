fb27f51834aed21f34e7b431a15bc1164e025c62
thanhxt <thanhtran_xuan@hotmail.de>
2026-01-30 20:09:18 +0100

    Add AWS Lambda Hosting and configuration

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
